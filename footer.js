d5bc6fb44c75137cfa1f5ec30b23491cd9caa646
sloth-folivora <157196026+sloth-folivora@users.noreply.github.com>
2024-01-20 18:42:17 +0900

    Add files via upload
    ヘッダ・フッタ作成。
    更新ログを別ファイルに移動。

---
+++ footer.js
@@ -0,0 +1,4 @@
+var html =  '<p class="pagetop">';
+    html += '  <a href="#" class="button-top">▲</a>';
+    html += '</p>';
+document.write(html);
---
+++ header.js
@@ -0,0 +1,7 @@
+var html =  '<header>';
+    html += '  <ul id="nav">';
+    html += '    <li><a href="index.html">積読状況</a></li>';
+    html += '    <li><a href="record_2024.html">2024年の記録</a></li>';
+    html += '  </ul>';
+    html += '</header>';
+document.write(html);
