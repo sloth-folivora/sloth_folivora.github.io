4c49fef9dd5d2ef6735fdc9a7e4c772053c49570
haruna <springspringrunrun@gmail.com.jp>
2024-02-15 00:06:23 +0900

    ～2/15　更新

--- html_js/update_log.js
+++ html_js/update_log.js
@@ -1,6 +1,10 @@
 var html =  '<div class="box">';
     html += '  <table class="upd">';
     html += '    <tr>';
+    html += '      <td class="upd">2024/02/15</td>';
+    html += '      <td class="upd">今月ページ：記録更新。（漫画・本・ドラマ）</td>';
+    html += '    </tr>';
+    html += '    <tr>';
     html += '      <td class="upd">2024/02/10</td>';
     html += '      <td class="upd">今月ページ：一週間分の記録更新。（漫画・本・映画）</td>';
     html += '    </tr>';
